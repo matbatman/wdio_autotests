0b08516babaa78c2c2871296f2bfe37c99d6a746
siringx23 <siringx23@gmail.com>
2025-08-19 21:15:27 +0300

    Написаны тесты на элемент поиска

--- ui/pageobjects/page.js
+++ ui/pageobjects/page.js
@@ -6,14 +6,18 @@ const { browser } = require('@wdio/globals')
 */
 module.exports = class Page {
 
-    SELECTORS= {
+    SELECTORS = {
         TAB_NAME: (elementMenu) => `//div[@class='supernav_container']//a[contains(text(), '${elementMenu}')]`,
+        SEARCH_BUTTON: '#store_search_link img',
+        SEARCH_RESULTS_ROWS: '#search_resultsRows',
+        STORE_SEARCH_INPUT: '#store_nav_search_term',
+        GAME_NAME: (someName) => `//span[@class='title'][text()='${someName}']`
     }
     /**
     * Opens a sub page of the page
     * @param path path of the sub page (e.g. /path/to/page.html)
     */
-    open (path) {
+    open(path) {
         return browser.url(`https://the-internet.herokuapp.com/${path}`)
     }
 }
